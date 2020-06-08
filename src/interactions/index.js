2c89234630a15c7232f8fb87bdcb7d336fce33ef
Roman Yakovlev <tensegrity666@icloud.com>
2020-06-08 20:45:36 +0300

    feat: add interaction interface

---
+++ src/interactions/index.js
@@ -0,0 +1,12 @@
+const interactions = [];
+
+const interactionsId = [];
+
+const interactionsMap = interactions.reduce((acc, InteractionCreater) => {
+  acc[InteractionCreater.id] = new InteractionCreater();
+  interactionsId.push(InteractionCreater.id);
+  return acc;
+}, {});
+
+
+export { interactionsMap, interactionsId };
---
+++ src/interactions/interface.js
@@ -0,0 +1,12 @@
+/* eslint-disable lines-between-class-members */
+
+class IAbstract {
+  object = null;
+  state = {};
+
+  preload() {}
+  create() {}
+  update() {}
+}
+
+export default IAbstract;
