a3a007343c2cf0f526bf23eb456ab76ad6a9c22c
Kate <braumwinter@gmail.com>
2020-06-21 15:51:12 +0300

    feat: add water

--- src/components/map/constants.js
+++ src/components/map/constants.js
@@ -2,11 +2,13 @@ import { nanoid } from 'nanoid';
 
 import map from './assets/liberty-city.json';
 import tileSet from './assets/gta-tiles.jpg';
+import waterSet from './assets/water-tiles.png';
 
 const PARAMS = {
   id: nanoid(),
   mapJSON: map,
   pic: tileSet,
+  waterPic: waterSet,
 };
 
 export default PARAMS;
--- src/components/map/index.js
+++ src/components/map/index.js
@@ -1,7 +1,5 @@
 /* eslint-disable no-unused-vars */
-import {
-  nanoid,
-} from 'nanoid';
+import { nanoid } from 'nanoid';
 
 import IAbstarct from '../interface';
 import PARAMS from './constants';
@@ -13,6 +11,10 @@ class Map extends IAbstarct {
   preload(scene) {
     scene.load.image(PARAMS.id, PARAMS.pic);
     scene.load.tilemapTiledJSON(this.constructor.id, PARAMS.mapJSON);
+    scene.load.spritesheet('waterSprite', PARAMS.waterPic, {
+      frameWidth: 64,
+      frameHeight: 64,
+    });
   }
 
   create(scene, featuresMap) {
@@ -20,9 +22,36 @@ class Map extends IAbstarct {
 
     this.object = scene.add.tilemap(this.constructor.id);
 
+    const config = {
+      key: 'waterAnimation',
+      frames: scene.anims.generateFrameNumbers('waterSprite', {
+        start: 43,
+        end: 45,
+      }),
+      frameRate: 1,
+      repeat: -1,
+      repeatDelay: 2,
+    };
+
+    scene.anims.create(config);
+
+    const waterLayer = this.object.layers[0].data;
+
+    for (let pointArr = 0; pointArr < waterLayer.length; pointArr++) {
+      for (let tile = 0; tile < waterLayer[pointArr].length; tile++) {
+        const tileInfo = waterLayer[pointArr][tile];
+        if (tileInfo.index !== -1) {
+          const water = scene.add
+            .sprite(tileInfo.x * 64, tileInfo.y * 64, 'waterSprite', 23)
+            .setOrigin(0, 0);
+
+          water.anims.delayedPlay(Math.random() * 3, 'waterAnimation');
+        }
+      }
+    }
+
     const terrain = this.object.addTilesetImage('gta-tiles', PARAMS.id);
 
-    this.object.createStaticLayer('water', [terrain], 0, 0);
     this.object.createStaticLayer('ground', [terrain], 0, 0);
     this.object.createStaticLayer('roads', [terrain], 0, 0);
 
