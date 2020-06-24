71d9194568b9c677fb93ca746ae25e40d4903d59
Roman Yakovlev <tensegrity666@icloud.com>
2020-06-24 22:01:55 +0300

    Revert "Revert "Merge branch 'dev' into bots""
    This reverts commit 439c470e3a4d12d5e875281a5db36f730e6b2b42.

--- src/components/map/constants.js
+++ src/components/map/constants.js
@@ -1,12 +1,14 @@
 import { nanoid } from 'nanoid';
 
-import tileSet from './assets/gta-tiles.jpg';
 import map from './assets/liberty-city.json';
+import tileSet from './assets/gta-tiles.jpg';
+import waterSet from './assets/water-tiles.png';
 
 const PARAMS = {
   id: nanoid(),
   mapJSON: map,
   pic: tileSet,
+  waterPic: waterSet,
 };
 
 export default PARAMS;
